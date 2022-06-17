33674261729ca75aed3cf8bf2d0e7b0d7cdb6f77
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-06-17 17:33:41 +0700

    feat: change to class

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-const PlayList = () => {
-    return (
-        <div className="TrackList">
-            {/* add a map method that renders a set of Track components */}
-        </div>
-    );
+class TrackList extends React.Component {
+    render() {
+        return (
+            <div className="TrackList">
+                {/* add a map method that renders a set of Track components */}
+                
+            </div>
+        );
+    }
 }
 
-export default PlayList;
+export default TrackList;
