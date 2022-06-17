e9049a9a88928f2ac8b6df76cc003c2c6e3eca88
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-06-17 17:34:52 +0700

    feat: change to class

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -2,13 +2,15 @@ import React from "react";
 import TrackList from "../TrackList/TrackList";
 import './SearchResults.css'
 
-const SearchResults = () => {
-    return (
-        <div className="SearchResults">
-            <h2>Results</h2>
-            <TrackList />
-        </div>
-    );
+class SearchResults extends React.Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                <TrackList />
+            </div>
+        );
+    }
 }
 
 export default SearchResults;
