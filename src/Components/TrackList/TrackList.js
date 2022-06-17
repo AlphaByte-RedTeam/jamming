440c4a1f868362fe8645df188acec03b9b5cd103
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-06-17 12:15:51 +0700

    feat: add TrackList

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlayList = () => {
+    return (
+        <div className="TrackList">
+            {/* add a map method that renders a set of Track components */}
+        </div>
+    );
+}
+
+export default PlayList;
