53bd208c702d9f1a44d52e8216226eb68240f2ca
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-06-17 11:44:28 +0700

    feat: add SearchResults

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './SearchResults.css'
+
+const SearchResults = () => {
+    <div className="SearchResults">
+        <h2>Results</h2>
+        {/* Add a TrackList component */}
+    </div>
+}
+
+export default SearchResults;
