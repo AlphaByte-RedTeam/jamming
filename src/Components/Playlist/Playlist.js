999ca4197a9b0376f7a396a3ffa45c6472f1e13d
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-06-17 14:07:54 +0700

    feat: add TrackList component

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,11 +1,12 @@
 import React from "react";
+import TrackList from "../TrackList/TrackList";
 import './Playlist.css';
 
 const Playlist = () => {
     return (
         <div className="Playlist">
             <input defaultValue={'New Playlist'} />
-            {/* Add a TrackList component */}
+            <TrackList />
             <button className="Playlist-save">SAVE TO SPOTIFY</button>
         </div>
     );
