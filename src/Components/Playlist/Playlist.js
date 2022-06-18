75da4385ee241e644cccfca18094c42e898ce348
Andrew Virya Victorio <andrew.avv03@gmail.com>
2022-06-18 07:10:44 +0700

    feat: pass data to Playlist

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -2,14 +2,16 @@ import React from "react";
 import TrackList from "../TrackList/TrackList";
 import './Playlist.css';
 
-const Playlist = () => {
-    return (
-        <div className="Playlist">
-            <input defaultValue={'New Playlist'} />
-            <TrackList />
-            <button className="Playlist-save">SAVE TO SPOTIFY</button>
-        </div>
-    );
+class Playlist extends React.Component {
+    render() {
+        return (
+            <div className="Playlist">
+                <input defaultValue={this.props.name} />
+                <TrackList tracks={this.props.tracks} />
+                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+            </div>
+        );
+    }
 }
 
 export default Playlist;
